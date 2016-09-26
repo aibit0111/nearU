18bb59cf8fd35b1d4ef4c0f1f1202b5edbab72a1
Mohit Kumar <Mohit Kumar>
2016-09-26 16:14:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,128 @@
+var fs = require('fs');
+var express = require('express');
+var https = require('https');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var path = require('path');
+var key = fs.readFileSync('./key.pem');
+var cert = fs.readFileSync('./cert.pem')
+var https_options = {
+  key: key,
+  cert: cert
+};
+var logger = require('morgan');
+var compression = require('compression');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var flash = require('express-flash');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var dotenv = require('dotenv');
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var PORT = 4500;
+var HOST = 'localhost';
+// Load environment variables from .env file
+dotenv.load();
+
+// Controllers
+var HomeController = require('./controllers/home');
+var userController = require('./controllers/user');
+var contactController = require('./controllers/contact');
+
+// Passport OAuth strategies
+require('./config/passport');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/boiler');
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
+
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: {
+    ifeq: function(a, b, options) {
+      if (a === b) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    },
+    toJSON : function(object) {
+      return JSON.stringify(object);
+    }
+  }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(methodOverride('_method'));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public')));
+0 
+app.get('/', HomeController.index);
+app.get('/contact', contactController.contactGet);
+app.post('/contact', contactController.contactPost);
+app.get('/account', userController.ensureAuthenticated, userController.accountGet);
+app.put('/account', userController.ensureAuthenticated, multer({ dest: './uploads/'}).single('upl'), userController.accountPut);
+app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
+app.get('/signup', userController.signupGet);
+app.post('/signup', userController.signupPost);
+app.get('/login', userController.loginGet);
+app.post('/login', userController.loginPost);
+app.get('/forgot', userController.forgotGet);
+app.post('/forgot', userController.forgotPost);
+app.get('/reset/:token', userController.resetGet);
+app.post('/reset/:token', userController.resetPost);
+app.get('/logout', userController.logout);
+app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/vkontakte', passport.authenticate('vkontakte', { scope: ['email'] }));
+app.get('/auth/vkontakte/callback', passport.authenticate('vkontakte', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email profile repo' ] }));
+app.get('/auth/github/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/search', userController.ensureAuthenticated, userController.searchget);
+app.post('/search', userController.ensureAuthenticated, function(req, res) {
+  db.collection('users').find({}, function (err, docs) {
+    res.json(docs);
+  });
+});
+app.get('/profile', userController.ensureAuthenticated, userController.profile);
+app.get('/messages', userController.ensureAuthenticated, userController.messages);
+app.get('/pingrequest', userController.ensureAuthenticated, userController.pingrequest);
+
+
+// Production error handler
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.sendStatus(err.status || 500);
+  });
+}
+
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+console.log("server is running");
+
+module.exports = app;
